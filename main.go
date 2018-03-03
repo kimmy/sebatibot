cc9b85cba61efe6d5820553a79e9711a92d5e9fe
Kim Leabres <kbleabres@gmail.com>
2018-03-03 20:21:42 +0800

    Add godotenv for handling environment variables

--- main.go
+++ main.go
@@ -8,15 +8,23 @@ import (
 	"github.com/markbates/going/defaults"
 	"sebatibot/app"
 	"sebatibot/logger"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	app := app.App()
+	log := logger.GetLogger()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	port := defaults.String(os.Getenv("GO_ENV_PORT"), "8000")
 
-	log := logger.GetLogger()
 	log.Debugf("Starting API server at %s", port)
+	log.Debugf("TOKEN: %s", os.Getenv("TOKEN"))
 
 	//Start the api here
 	http.ListenAndServe(fmt.Sprintf(":%s", port), app)
